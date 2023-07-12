2586272c1b85d6ab945207f02a56f909e3cb2a3e
Anubhav Kumar <66882903+findanubhav420@users.noreply.github.com>
2023-07-12 15:17:29 +0530

    indexk

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const bookingRouter = require('./src/routes/bookingRoutes.js');
 const flightRouter = require('./src/routes/flightRoutes.js');
 
 app.use(bookingRouter);
 app.use(flightRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 const port = 3000;
 
