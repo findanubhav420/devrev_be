bd0ff09037b7352f46954d8a2556ca026c3405ea
Anubhav Kumar <66882903+findanubhav420@users.noreply.github.com>
2023-07-12 14:05:21 +0530

    test

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
